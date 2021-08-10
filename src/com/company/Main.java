daf713f1662b018a642121c37296d1c3b40dd5a0
HectorAVC <87152200+HectorAVC@users.noreply.github.com>
2021-08-10 17:29:06 -0600

    Tarea 1.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,26 +6,32 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
-    	List<Figure> figuras = new ArrayList<>();
-	    Figure triangulo1 = new Triangulo();
+    public static void main (String[] args) {
+		List<Figure> figuras = new ArrayList<>();
+		Figure triangulo1 = new Triangulo();
 		Figure triangulo2 = new Triangulo();
-	    Figure cuadro1 = new Cuadrado();
+		Figure cuadro1 = new Cuadrado();
 		figuras.add(triangulo1);
 		figuras.add(triangulo2);
 		figuras.add(cuadro1);
-	    triangulo1.getName();
+		triangulo1.getName();
 		System.out.println(triangulo1.getName());
 		System.out.println(triangulo1.getArea(10, 9));
 		System.out.println(triangulo1.toString());
 		System.out.println(triangulo2.toString());
 		System.out.println(cuadro1.getName());
-		System.out.println(cuadro1.getArea(10,10));
+		System.out.println(cuadro1.getArea(10, 10));
 		System.out.println(Cuadrado.HOW_MANY_SIDES);
 		System.out.println(Cuadrado.getInformation());
 		System.out.println(Math.PI);
 		String name = JOptionPane.showInputDialog("Como te llamas?");
 		String age = JOptionPane.showInputDialog("cuantos años tienes?");
-		JOptionPane.showMessageDialog(null, "Hola " + name + " tienes "  + (Integer.parseInt(age) * 365)  + " dias");
-    }
+		JOptionPane.showMessageDialog(null, "Hola " + name + " tienes " + (Integer.parseInt(age) * 365) + " dias");
+		String adress = JOptionPane.showInputDialog("¿Cuál es tu dirección?");
+		String Hobby = JOptionPane.showInputDialog("Cuál es tu Hobby?");
+		String Height = JOptionPane.showInputDialog("Cual es tu estatura?");
+		String Weight = JOptionPane.showInputDialog("Cual es tu peso?");
+
+	}
 }
+
---
+++ src/com/company/class2/Person.java
@@ -0,0 +1,10 @@
+package com.company.class2;
+
+public class Person {
+    private String name;
+    private String age;
+    private String address;
+    private String hobby;
+    private String height;
+    private String weight;
+}
