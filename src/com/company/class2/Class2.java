665da70907c72a4b804da88600c64c6984ea8bbe
Obed Murillo <obedmhg@gmail.com>
2021-08-20 17:45:28 -0600

    Adding class3 FlowCotrolExamples

--- src/com/company/class2/Class2.java
+++ src/com/company/class2/Class2.java
@@ -10,21 +10,21 @@ public class Class2 {
             String age = JOptionPane.showInputDialog("cuantos años tienes?");
             MyJOptionPane.showMessageDialog(null, "Hola " + name + " tienes " + (Double.parseDouble(age) * 365) + " dias");
             JOptionPane.showMessageDialog(null, "Hola " + name + " tienes " + (Double.parseDouble(age) * 365) + " dias", "Here is the title", 2);
+            String address = JOptionPane.showInputDialog("¿Cuál es tu dirección?");
+            String hobby = JOptionPane.showInputDialog("Cuál es tu Hobby?");
+            String height = JOptionPane.showInputDialog("Cual es tu estatura?");
+            String weight = JOptionPane.showInputDialog("Cual es tu peso?");
+            Person person = new Person();
+            person.setName(name);
+            person.setAge(age);
+            person.setAddress(address);
+            person.setHobby(hobby);
+            person.setHeight(height);
+            person.setWeight(weight);
+            JOptionPane.showMessageDialog(null, person.toString());
         } catch(NumberFormatException e) {
             JOptionPane.showMessageDialog(null, "Hola parece que algun dato no es correcto");
-        }String address = JOptionPane.showInputDialog("¿Cuál es tu dirección?");
-        String Hobby = JOptionPane.showInputDialog("Cuál es tu Hobby?");
-        String Height = JOptionPane.showInputDialog("Cual es tu estatura?");
-        String Weight = JOptionPane.showInputDialog("Cual es tu peso?");
-        Person person = new Person();
-        String name;
-        person.setName(name);
-        person.setAge(age);
-        person.setAddress(address);
-        person.setHobby(hobby);
-        person.setHeight(string Height);
-        person.setWeight(string weight);
-        JOptionPane.showMessageDialog(null, person.toString());
+        }
 
     }
 
