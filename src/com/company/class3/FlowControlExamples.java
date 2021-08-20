17761b83d8fe84112a764d65ba80aa28dfddf05a
Obed Murillo <obedmhg@gmail.com>
2021-08-20 17:46:17 -0600

    sout update

--- src/com/company/class3/FlowControlExamples.java
+++ src/com/company/class3/FlowControlExamples.java
@@ -22,8 +22,7 @@ public class FlowControlExamples {
 
         for (int i=0; i<3; i++) {
             for (int j=0; j<10; j++) {
-                System.out.println("i=" +i);
-                System.out.println("j=" +j);
+                System.out.println("i=" +i + ",j=" +j);
             }
         }
 
